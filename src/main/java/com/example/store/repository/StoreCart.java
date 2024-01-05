d4d4d570e4e9f1a7d4486d2375d28802d5ff9e22
Юлия <Yulechka140200@gmail.com>
2024-01-05 23:26:23 +0500

    готово

--- src/main/java/com/example/store/repository/StoreCart.java
+++ src/main/java/com/example/store/repository/StoreCart.java
@@ -11,29 +11,18 @@ import java.util.*;
 @SessionScope
 public class StoreCart {
 
-//    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final List<Integer> products;
     public StoreCart() {
         this.products = new ArrayList<>();
-//        LOGGER.info("Store card is created.");
     }
 
     @NonNull
     public List<Integer> addProduct(List<Integer> id) {
-//        Arrays.stream(id)
-//                .forEach((producttId -> {
-//                    if (products.containsKey(producttId)) {
-//                        return;
-//                    }
-//                    products.put(producttId, new StoreProduct(producttId));
-//                }));
         this.products.addAll(id);
         return  this.products;
-//        LOGGER.info("Product(s) added in cart.");
     }
 
         public List<Integer> getProducts() {
-//        LOGGER.info("Product list is got");
         return products;
     }
 }
